90b7b03411f7d170d21e830b282a319c5503c9dd
cat4m4r4n <leece2121@gmail.com>
2018-05-15 11:20:08 -0700

    Add files via upload

---
+++ kendall-tau-p.R
@@ -0,0 +1,30 @@
+#kendall tau significance uses z score
+#calculate z score
+
+#function to calculate z from kendall's tau
+tau_to_z <- function(n, tau, alpha){
+  #tau to z
+  numerator <- 3 * tau * (sqrt(n*(n-1)))
+  denominator <- (sqrt(2*(2*n+5)))
+  z <- numerator/denominator
+  #z to p crit
+  z.half.alpha = qnorm(1-alpha/2)
+  pcrit <-c(-z.half.alpha, z.half.alpha)
+  #pval two-tailed
+  pval <- 2 * pnorm(z, lower.tail=FALSE)
+  #generate significance stars
+  stars <- ifelse(pval < .001, "***", ifelse(pval < .01, "**", ifelse(pval < .05, "*", ""))) 
+  starstext <-"p < .0001 ‘****’; p < .001 ‘***’, p < .01 ‘**’, p < .05 ‘*’"
+  #save to list to return multiple objects. lefthand is the label
+  results <-list(zscore=z, pcrit=pcrit, pval=paste((round(pval,4)),stars), sig=starstext)
+  return(results)
+}
+
+#test
+#tau_to_z(n=44, tau=0.164, alpha = 0.05)
+
+#kendall's tau is (concordant num of pairs - num of discordant pairs) / (num concordant pairs + num of discordant pairs)
+#z = (3*tau*(sqrt(n(n-1))))/(sqrt(2(2n+5)))
+
+cor.test(var1, var2, method = "kendall")
+#accepts dataset instead of vars
